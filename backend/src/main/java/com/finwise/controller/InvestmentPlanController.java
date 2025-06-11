6fb80d0fcaba76de50a4dae1be96f6db9a38cacd
Akash donge <114501810+Akashdonge@users.noreply.github.com>
2025-06-11 13:56:53 +0530

    Update InvestmentPlanController.java

--- backend/src/main/java/com/finwise/controller/InvestmentPlanController.java
+++ backend/src/main/java/com/finwise/controller/InvestmentPlanController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/investment-plans")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+
 public class InvestmentPlanController {
 
     private final InvestmentPlanService investmentPlanService;
